5f5758e23b6d676b86f6e401e5f2716c20c208d1
iloktionov <iloktionov@skbkontur.ru>
2020-02-01 21:15:57 +0500

    Implemented ConfigurableLogBuilder.

--- Vostok.Logging.Configuration/ConfigurableLogBuilder.cs
+++ Vostok.Logging.Configuration/ConfigurableLogBuilder.cs
@@ -1,10 +1,54 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using Vostok.Commons.Helpers.Observable;
+using Vostok.Logging.Abstractions;
+using Vostok.Logging.Configuration.Helpers;
 
 namespace Vostok.Logging.Configuration
 {
     [PublicAPI]
     public class ConfigurableLogBuilder
     {
+        private readonly Dictionary<string, ILog> baseLogs;
+        private volatile IObservable<LogConfigurationRule[]> rulesSource;
 
+        public ConfigurableLogBuilder()
+        {
+            baseLogs = new Dictionary<string, ILog>(StringComparer.OrdinalIgnoreCase);
+            rulesSource = new CachingObservable<LogConfigurationRule[]>(Array.Empty<LogConfigurationRule>());
+        }
+
+        [NotNull]
+        public ConfigurableLog Build()
+            => new ConfigurableLog(baseLogs, rulesSource);
+
+        [NotNull]
+        public ConfigurableLogBuilder AddLog([NotNull] string name, [NotNull] ILog log)
+        {
+            baseLogs[name] = log ?? throw new ArgumentNullException(nameof(log));
+            return this;
+        }
+
+        [NotNull]
+        public ConfigurableLogBuilder SetRules([NotNull] LogConfigurationRule[] rules)
+        {
+            rulesSource = new CachingObservable<LogConfigurationRule[]>(rules);
+            return this;
+        }
+
+        [NotNull]
+        public ConfigurableLogBuilder SetRules([NotNull] Func<LogConfigurationRule[]> rulesProvider, TimeSpan updatePeriod)
+        {
+            rulesSource = new PeriodicObservable<LogConfigurationRule[]>(rulesProvider, updatePeriod);
+            return this;
+        }
+
+        [NotNull]
+        public ConfigurableLogBuilder SetRules([NotNull] IObservable<LogConfigurationRule[]> rulesProvider)
+        {
+            rulesSource = rulesProvider ?? throw new ArgumentNullException(nameof(rulesProvider));
+            return this;
+        }
     }
 }
---
+++ Vostok.Logging.Configuration/Helpers/PeriodicObservable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Vostok.Commons.Helpers.Observable;
+using Vostok.Commons.Time;
+
+namespace Vostok.Logging.Configuration.Helpers
+{
+    internal class PeriodicObservable<T> : IObservable<T>, IDisposable
+    {
+        private readonly CachingObservable<T> baseObservable;
+        private readonly PeriodicalAction updateAction;
+
+        public PeriodicObservable(Func<T> valueProvider, TimeSpan period)
+        {
+            baseObservable = new CachingObservable<T>();
+            updateAction = new PeriodicalAction(() => baseObservable.Next(valueProvider()), _ => {}, () => period);
+        }
+
+        public void Dispose()
+            => updateAction.Stop();
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            updateAction.Start();
+
+            return baseObservable.Subscribe(observer);
+        }
+    }
+}
